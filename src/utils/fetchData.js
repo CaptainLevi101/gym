93d407caf970c5e7b461c5d3621feb9688d6a49f
Ashish Parashar <200104016@hbtu.ac.in>
2023-09-08 22:52:26 +0530

    updated

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'c0c92c62cdmsh641c66adc5879fcp1ea010jsn0b2ea405021c',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
+  
